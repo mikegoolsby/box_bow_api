4413350f85306bc3d75f3cc3413bbc7a89cb7bcd
Michael Goolsby <mgoolsby1091@gmail.com>
2020-12-09 09:55:48 -0500

    adds first name to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_235731) do
+ActiveRecord::Schema.define(version: 2020_12_09_144353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_12_08_235731) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
   end
 
   add_foreign_key "gifts", "users"
